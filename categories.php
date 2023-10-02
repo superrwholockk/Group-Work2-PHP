fd69b7e3581ba3d5b0d7b0def8754d7dea07397e
NIKOLOZ TSIREKIDZE <142229511+zolokin1@users.noreply.github.com>
2023-10-02 16:08:18 +0400

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,10 @@
+<li class="nav-item">
+                    <a class="nav-link" href="treatment.html">Treatment</a>
+                    <ul class="category">
+                      <li> Nephrologist Care</li>
+                      <li> Eye Care</li>
+                      <li> Pediatrician Clinic</li>
+                      <li> Parental Care</li>
+                    </ul>
+                  </li>
+                  
